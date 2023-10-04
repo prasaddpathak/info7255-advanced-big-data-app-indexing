f0a1628db62f33056d272ab147d3ee1258b65e55
Prasad Pathak <prasaddpathak@gmail.com>
2023-10-04 00:52:46 -0700

    fixed redis import error
    Using ioredis instead as its more ES6 friendly

--- src/controller/plan-controller.js
+++ src/controller/plan-controller.js
@@ -1,41 +1,37 @@
-
 import jsonschema from 'jsonschema';
-// import redis from 'redis';
+import plan from './../schema/plan.js';
+
 
 const val = new jsonschema.Validator();
 
-import plan from './../schema/plan.js';
+import Redis from 'ioredis';
+const client = new Redis();
 
-// const client = redis.createClient();
-
-export const post = (req, res) => {
+export const post = async (req, res) => {
     console.log('------------------------------------')
     console.log(Date().toString() + ' :: Received POST: /v1/plan')
-    // const result = val.validate(req.body, plan);
-    // if (!result.valid) return res.status(400).send(result.errors);
-    // client.set(req.body.objectId, JSON.stringify(req.body), redis.print);
-    // res.send('Data saved successfully');
-    res.sendStatus(201);
+    const result = val.validate(req.body, plan);
+    if (!result.valid) return res.status(400).send(result.errors);
+    client.set(req.body.objectId, JSON.stringify(req.body));
+    res.send('Data saved successfully');
 }
 
 export const get = (req, res) => {
     console.log('------------------------------------')
     console.log(Date().toString() + ' :: Received GET: /v1/plan/:id')
-    // client.get(req.params.id, (err, reply) => {
-    //     if (err) return res.status(500).send(err);
-    //     if (reply) return res.send(JSON.parse(reply));
-    //     res.status(404).send('Data not found');
-    // });
-    res.sendStatus(202);
+    client.get(req.params.id, (err, reply) => {
+        if (err) return res.status(500).send(err);
+        if (reply) return res.send(JSON.parse(reply));
+        res.status(404).send('Data not found');
+    });
 }
 
 export const del = (req, res) => {
     console.log('------------------------------------')
     console.log(Date().toString() + ' :: Received DELETE: /v1/plan/:id')
-    // client.del(req.params.id, (err, reply) => {
-    //     if (err) return res.status(500).send(err);
-    //     if (reply) return res.send('Data deleted successfully');
-    //     res.status(404).send('Data not found');
-    // });
-    res.sendStatus(203);
+    client.del(req.params.id, (err, reply) => {
+        if (err) return res.status(500).send(err);
+        if (reply) return res.send('Data deleted successfully');
+        res.status(404).send('Data not found');
+    });
 }
